0870e06a95ebb8b41b58843b2f30056f2e7d89f7
h4n5o <72876852+h4n5o@users.noreply.github.com>
2024-02-19 19:42:21 +0100

    API

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require ("morgan");
+const userRoute = require("./Routes/users");
+const authRoute = require("./Routes/auth");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, )
+    .then(() => console.log('Connected Successfully'))
+    .catch((err) => { console.error(err); 
+});
+
+//Middleware//
+app.use(express.json());
+app.use(morgan("common"));
+app.use(helmet());
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.listen(3000,()=>{
+    console.log("Backend Server is running!")
+});
